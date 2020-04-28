89bd8fac5c84564a47d51334ca9dfb45bcc72f45
Edo Novanto <novantoedo@gmail.com>
2020-04-29 02:48:18 +0700

    map complete

--- app/src/main/java/com/novanto/covid/Maps.java
+++ app/src/main/java/com/novanto/covid/Maps.java
@@ -1,6 +1,7 @@
 package com.novanto.covid;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,13 +9,59 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class Maps extends Fragment {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Maps extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mapAPI;
+    SupportMapFragment mapFragment;
+    View mView;
+
+    public Maps(){
+
+    }
 
     @Nullable
-    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.maps_frag, container, false);
+        View v = inflater.inflate(R.layout.maps_frag, container, false);
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if(mapFragment == null){
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            mapFragment = SupportMapFragment.newInstance();
+            ft.replace(R.id.map,mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+        return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mapAPI = googleMap;
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        LatLng bekauheni = new LatLng(-6.175110,106.865036);
+
+        mapAPI.addMarker(new MarkerOptions().position(bekauheni).title("Total Kasus : 9542"));
+
+        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(bekauheni));
+
     }
 }
