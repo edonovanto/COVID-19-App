7787e97739b83ea7eebcecdc916a6b0cc8d1e169
hananhanafi <hananhanafi@mail.ugm.ac.id>
2020-05-10 22:08:46 +0700

    Create covid19api.com interface

--- app/src/main/java/com/novanto/covid/cases/Covid19Api.java
+++ app/src/main/java/com/novanto/covid/cases/Covid19Api.java
@@ -1,9 +1,12 @@
 package com.novanto.covid.cases;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface Covid19Api {
 
     String BASE_URL = "https://api.covid19api.com/";
 
+    @GET("summary")
+    Call<Covid19Api> getAllCovid19Cases();
 }
